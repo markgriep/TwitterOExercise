9654632077fc215191ab84d6fabc03e7f3f3ea0f
Mark Griep <megriep@gmail.com>
2023-02-03 09:58:50 -0600

    Initial commit with boiler plate stuff

---
+++ TwitterStatsTerminal/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TwitterStatsTerminal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //Wheew, where to start.  6 Hours?!?! Seriously? 
+        }
+    }
+}
